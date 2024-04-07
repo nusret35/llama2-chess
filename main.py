084e0b30fd0057f439a2174457bec47824fdd054
Nusret Ali Kizilaslan <nusret35@users.noreply.github.com>
2024-04-07 18:32:18 +0300

    seperated player types and added error handling for invalid moves

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import chess
-from schemas import Game, Player
-from llm_player import LLMPlayer
+from schemas import Game, HumanPlayer, LLMPlayer
 from dotenv import load_dotenv
 
 
 if __name__ == "__main__":
     load_dotenv()
 
-    player_white, player_black = Player(chess.WHITE), LLMPlayer(chess.BLACK)
+    player_white, player_black = HumanPlayer(chess.WHITE), LLMPlayer(chess.BLACK)
     game = Game(player_white,
                 player_black)
     
